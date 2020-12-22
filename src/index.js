afe286915dc009ca8d62e8346e4fd6df7be78e35
Evgeniya3010 <susskaya86@gmail.com>
2020-12-22 04:21:35 +0200

    creates template and markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles.css';
+
+import menuTemplate from './templates/menu-items.hbs';
+
+import menuList from './menu.json';
+console.log(menuList);
+
+const markup = menuTemplate(menuList);
+console.log(markup);
+
+const menuRef = document.querySelector('.js-menu');
+
+menuRef.insertAdjacentHTML('beforeend', markup);
