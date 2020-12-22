56f63864c5c4f8bfd85d80717797668a7d1e6b42
Evgeniya3010 <susskaya86@gmail.com>
2020-12-22 08:37:50 +0200

    realization of changing theme

--- src/index.js
+++ src/index.js
@@ -15,10 +15,43 @@ menuRef.insertAdjacentHTML('beforeend', markup);
 
 // переключение темы
 
+const bodyRef = document.querySelector('body');
+const themeRef = document.querySelector('#theme-switch-toggle');
+
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
-localStorage.setItem('Theme', JSON.stringify(Theme));
-localStorage.removeItem('Theme');
+// themeRef.addEventListener('change', themeRefHandler);
+// console.log(themeRefHandler);
+
+const themeRefHandler = event => {
+  if (themeRef.checked) {
+    bodyRef.classList.add(Theme.DARK);
+    bodyRef.classList.remove(Theme.LIGHT);
+
+    localStorage.setItem('index', JSON.stringify(Theme.DARK));
+    localStorage.removeItem('index', JSON.stringify(Theme.LIGHT));
+  } else if (!themeRef.checked) {
+    bodyRef.classList.replace(Theme.DARK, Theme.LIGHT);
+
+    localStorage.removeItem('index', JSON.stringify(Theme.DARK));
+    localStorage.setItem('index', JSON.stringify(Theme.LIGHT));
+  }
+};
+
+themeRef.addEventListener('change', themeRefHandler);
+console.log(themeRefHandler);
+
+const loadTheme = () => {};
+
+const saveTheme = () => {
+  // if (bodyRef.classList) {
+};
+
+// themeRef.addEventListener('change', themeRefHandler);
+// console.log(themeRefHandler);
+
+// localStorage.setItem('Theme', JSON.stringify(Theme));
+// localStorage.removeItem('Theme');
