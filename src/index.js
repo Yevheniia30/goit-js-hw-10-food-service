f76a60ad9aae29e9b9bfd4f5ae0de6898a4ee41d
Evgeniya3010 <susskaya86@gmail.com>
2020-12-22 05:23:30 +0200

    starts writing function of changes theme

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles.css';
 
+// -----создание шаблона и разметки-----
 import menuTemplate from './templates/menu-items.hbs';
 
 import menuList from './menu.json';
@@ -11,3 +12,13 @@ console.log(markup);
 const menuRef = document.querySelector('.js-menu');
 
 menuRef.insertAdjacentHTML('beforeend', markup);
+
+// переключение темы
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+localStorage.setItem('Theme', JSON.stringify(Theme));
+localStorage.removeItem('Theme');
